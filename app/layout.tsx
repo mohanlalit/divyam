9abcce46203d217bfa8bdf899a7ad80d10876da2
Divyam Chandel <divyamchandel@Divyams-MacBook-Pro.local>
2024-12-25 23:59:39 +0530

    metadata changes:

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,15 +11,15 @@ import { baseUrl } from './sitemap'
 export const metadata: Metadata = {
   metadataBase: new URL(baseUrl),
   title: {
-    default: 'Next.js Portfolio Starter',
-    template: '%s | Next.js Portfolio Starter',
+    default: 'Lalit Mohan Blog',
+    template: '%s | Lalit Mohan Blog',
   },
   description: 'This is my portfolio.',
   openGraph: {
-    title: 'My Portfolio',
+    title: 'Lalit Mohan',
     description: 'This is my portfolio.',
     url: baseUrl,
-    siteName: 'My Portfolio',
+    siteName: 'Lalit Mohan',
     locale: 'en_US',
     type: 'website',
   },
